1877681987f5c1adaed33b3b04da39fc227297aa
Swastik2442 <kul.swas321tik@gmail.com>
2021-08-27 09:54:26 +0530

    Bootstrap Initial Setup

---
+++ script.js
@@ -0,0 +1,6 @@
+function loadingGG() {
+    setTimeout(function () {
+        document.getElementById("aload").style = "";
+        document.getElementById("bload").style = "display: none;";
+    }, 2500);
+}
